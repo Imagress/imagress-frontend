a4782a4fca2e935588eaa639805cebf5f48b21a4
nutchanon-c <nutchanon.char@gmail.com>
2022-11-01 13:58:45 +0700

    feat: upload button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import FileUploader from "./components/file-uploader";
+import ImagressLogo from "./components/imagress-logo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex flex-col justify-center items-center overflow-clip">
+      <ImagressLogo />
+      <FileUploader
+        handleFile={(file) => {
+          console.log(`file: ${file}`);
+        }}
+      />
     </div>
   );
 }
