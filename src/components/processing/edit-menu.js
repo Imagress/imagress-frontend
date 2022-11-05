c72e1d4f030368ac7995b30af11a38d15a0e867d
nutchanon-c <nutchanon.char@gmail.com>
2022-11-05 15:11:43 +0700

    feat: edit menu value handling

--- src/components/processing/edit-menu.js
+++ src/components/processing/edit-menu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FlipHorizontalIcon from "../../svg/flip-horizontal.svg";
 import FlipVerticalIcon from "../../svg/flip-vertical.svg";
 import ResizeIcon from "../../svg/resize.svg";
@@ -27,7 +27,7 @@ const EditMenu = (props) => {
     file: props.file,
     flipHorizontal: false,
     flipVertical: false,
-    resize: { width: null, height: null },
+    resize: { width: 0, height: 0 },
     sharpen: 0,
     median: false,
     blur: 0,
@@ -49,18 +49,44 @@ const EditMenu = (props) => {
     // console.log("resize");
     // console.log(`${e.target.id} ${e.target.value}`);
     let val = e.target.value;
-    let tmp = { ...options };
-    switch (e.target.id) {
-      case "width":
-        tmp.resize.width = val;
-        break;
-      case "height":
-        tmp.resize.height = val;
-        break;
-      default:
-        break;
+    if (parseInt(val) >= 0 || val === "") {
+      let tmp = { ...options };
+      switch (e.target.id) {
+        case "width":
+          tmp.resize.width = val;
+          break;
+        case "height":
+          tmp.resize.height = val;
+          break;
+        default:
+          break;
+      }
+      setOptions(tmp);
     }
-    setOptions(tmp);
+  };
+
+  const handleTintChange = (e) => {
+    console.log(e.target.id);
+    let id = e.target.id;
+    let value = e.target.value;
+    let tmp = { ...options };
+    if (parseInt(value) <= 255 || value === "") {
+      switch (id) {
+        case "tintR":
+          tmp.tint.r = value;
+          break;
+
+        case "tintG":
+          tmp.tint.g = value;
+          break;
+        case "tintB":
+          tmp.tint.b = value;
+          break;
+        default:
+          break;
+      }
+      setOptions(tmp);
+    }
   };
 
   const handleSliderChange = (e) => {
@@ -71,10 +97,27 @@ const EditMenu = (props) => {
       case "sharpen":
         tmp.sharpen = val;
         break;
+      case "blur":
+        tmp.blur = val;
+        break;
       default:
         break;
     }
     setOptions(tmp);
+  };
+
+  const toggleMenu = (menu) => {
+    let tmp = { ...showMenu };
+    Object.keys(tmp).forEach((v) => {
+      // console.log(v);
+      if (v !== menu) {
+        tmp[v] = false;
+      } else {
+        tmp[menu] = !tmp[menu];
+      }
+    });
+    // console.log(tmp);
+    setShowMenu(tmp);
   };
 
   return (
@@ -113,24 +156,27 @@ const EditMenu = (props) => {
       />
       <IconMenuButton
         icon={
-          options.resize.width || options.resize.height
+          (options.resize.width > 0 && options.resize.width) ||
+          (options.resize.height && options.resize.height > 0)
             ? ResizeIcon
             : ResizeDisabledIcon
         }
         alt="resize"
         onClick={() => {
-          let showTmp = { ...showMenu };
-          showTmp.resize = !showTmp.resize;
-          setShowMenu(showTmp);
+          toggleMenu("resize");
+          // let showTmp = { ...showMenu };
+          // showTmp.resize = !showTmp.resize;
+          // setShowMenu(showTmp);
         }}
       />
       <IconMenuButton
         icon={options.sharpen > 0 ? SharpenIcon : SharpenDisabledIcon}
         alt="sharpen"
         onClick={() => {
-          let tmp = { ...showMenu };
-          tmp.sharpen = !tmp.sharpen;
-          setShowMenu(tmp);
+          toggleMenu("sharpen");
+          // let tmp = { ...showMenu };
+          // tmp.sharpen = !tmp.sharpen;
+          // setShowMenu(tmp);
         }}
       />
       <IconMenuButton
@@ -145,6 +191,12 @@ const EditMenu = (props) => {
       <IconMenuButton
         icon={options.blur > 0 ? BlurIcon : BlurDisabledIcon}
         alt="blur"
+        onClick={() => {
+          toggleMenu("blur");
+          // let tmp = { ...showMenu };
+          // tmp.blur = !tmp.blur;
+          // setShowMenu(tmp);
+        }}
       />
       <IconMenuButton
         icon={options.flatten ? FlattenIcon : FlattenDisabledIcon}
@@ -180,6 +232,9 @@ const EditMenu = (props) => {
             : TintDisabledIcon
         }
         alt="tint"
+        onClick={() => {
+          toggleMenu("tint");
+        }}
       />
       <IconMenuButton
         icon={options.greyscale ? GrayscaleIcon : GrayscaleDisabledIcon}
@@ -210,6 +265,22 @@ const EditMenu = (props) => {
           />
         </div>
       )}
+      {showMenu.blur && (
+        <div className="absolute top-16 left-[300px] bg-[#215FF6] w-[200px] h-[40px] justify-center items-center flex rounded-xl">
+          <SliderMenu
+            min={0}
+            max={100}
+            value={options.blur}
+            onChange={handleSliderChange}
+            id="blur"
+          />
+        </div>
+      )}
+      {showMenu.tint && (
+        <div className="absolute top-16 left-[575px]">
+          <TintMenu value={options.tint} onChange={handleTintChange} />
+        </div>
+      )}
     </div>
   );
 };
@@ -224,6 +295,8 @@ const InputBox = (props) => {
         className="w-[60px] h-[45px] text-black rounded-md focus:border-0 bg-[#D9D9D9] appearance-none px-2"
         onChange={props.onChange}
         value={props.value}
+        min={props.min ?? 0}
+        max={props.max}
       />
     </div>
   );
@@ -282,6 +355,7 @@ const ResizeMenu = (props) => {
         text="Width"
         onChange={props.onChange}
         value={props.wVal}
+        min="0"
       />
       <InputBox
         type="number"
@@ -289,6 +363,41 @@ const ResizeMenu = (props) => {
         text="Height"
         onChange={props.onChange}
         value={props.hVal}
+        min="0"
+      />
+    </div>
+  );
+};
+
+const TintMenu = (props) => {
+  return (
+    <div className="w-[200px] h-[200px] bg-[#215FF6] flex flex-col gap-3 items-center justify-center rounded-xl">
+      <InputBox
+        type="number"
+        id="tintR"
+        text="R"
+        onChange={props.onChange}
+        value={props.value.r}
+        min="0"
+        max="255"
+      />
+      <InputBox
+        type="number"
+        id="tintG"
+        text="G"
+        onChange={props.onChange}
+        value={props.value.g}
+        min="0"
+        max="255"
+      />
+      <InputBox
+        type="number"
+        id="tintB"
+        text="B"
+        onChange={props.onChange}
+        value={props.value.b}
+        min="0"
+        max="255"
       />
     </div>
   );
